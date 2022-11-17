d0e3aa956cd2ba7f386ff958dd73fc50a77549b8
Eric Simmons <erictomlinsonsimmons@gmail.com>
2022-11-17 14:03:52 -0600

    api routes

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,33 @@
 const router = require('express').Router();
+const path = require('path')
+const fs = require('fs')
 const { notes } = require('../../db/db.json')
 const { createNote, editNote, removeNote } = require('../../lib/notes.js')
 
 router.use(notesRoutes);
 
-router.get('./notes', (req,res) =>{
+router.get('/api/notes', (req,res) =>{
     res.json(notes)
 })
 
-router.post('/notes', (req,res) => {
+router.post('/api/notes', (req,res) => {
     createNote(req.body, notes);
     res.json(req.body)
+})
+
+
+//post note and add to db.json
+router.post('/api/notes', (req, res) => {
+
+    console.log(req)
+    //gets new note from the req object
+    const newNote = req.body
+    //push into 'db' array
+   notes.push(newNote)
+    //update db with new stringified object
+    fs.writeFileSync(__dirname, '../db/db.json', JSON.stringify(notes))
+
+    res.json(notes)
 })
 
 module.exports = router;
