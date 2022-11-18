7c8ffc1ee727cbff2760edc8f9a51d79b8995fd3
Eric Simmons <erictomlinsonsimmons@gmail.com>
2022-11-18 10:37:38 -0600

    PORT for heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const routes = require('./routes')
 // const db = require('../db/db.json')
 
